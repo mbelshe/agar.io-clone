09fac18d2b7b88d72028baa32d83b8d16a2792a9
Aniketh <user@Users-MacBook-Pro.local>
2016-06-21 13:57:07 -0700

    Fix broken merge of webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 var path = require('path');
@@ -46,22 +45,4 @@ module.exports = {
       loader: 'file?name=[path][name].[ext]'
     }]
   }
-=======
-module.exports = {
-    entry: "./src/client/js/app.js",
-    output: {
-        path: require("path").resolve("./src/bin/client/js"),
-        library: "app",
-        filename: "app.js"
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel'
-            }
-        ]
-    }
->>>>>>> 23a2ceb208121a477b4717f71571dd1ed0fe1f24
 };
